6c6b56cfd6ee4d5a4f3dce7db20567ed5ecc5b1c
Conner Gieg <giegconner@gmail.com>
2021-04-06 22:09:24 +0000

    Added a test to verify that a username with at least one special char gets rejected

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -37,6 +37,16 @@ class TestValidator(unittest.TestCase):
 
         # Assert
         self.assertFalse(result)
+        
+    def test_it_will_reject_username_if_there_is_special_char(self):
+        # Assume
+        username = "rev$al"
+        
+        # Action
+        result = self.validator.username_is_valid(username)
+        
+        # Assert
+        self.assertFalse(result)
 
     def test_it_will_accept_a_valid_username(self):
         # Assume
